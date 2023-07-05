397502b4f1e8966ed17e6f0dc619feb897c2d567
Alirezababrgir <zakhargithub021@gmail.com>
2023-07-05 12:19:06 -0400

    serach box byb (useserachparams and uselocation) hooks -65

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,24 +1,37 @@
-import { NavLink,Outlet} from "react-router-dom";
+import { NavLink, Outlet, useSearchParams, useLocation } from "react-router-dom";
 import { Data } from "../data/data";
 const Books = () => {
+    const [serachparams, setserachparams] = useSearchParams();
+    const location=useLocation();
     const mydata = Data();
     return (
         <div className="container">
             <div className="row">
                 <div className="col">
                     <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">جستجو</button>
+                        <input className="form-control me-2" type="search" placeholder="کتاب مورد نظر ..." aria-label="Search"
+                            value={serachparams.get("filter" || "")}
+                            onChange={Event => {
+                                var filter = Event.target.value;
+                                if (filter) { setserachparams({ filter: filter }) }
+                                else { setserachparams({}) }
+                            }}
+                        />
                     </form>
 
                 </div>
                 <div className="col">
-                    {mydata.map((data) => (
-                        <NavLink to={`/books/${data.number}`} key={data.number}style={({isActive})=>{
-                            return{
-                                textDecoration:"none",
-                                display:"block",
-                                color:isActive?"red":""
+                    {mydata.filter((data) => {
+                        var filter = serachparams.get("filter");
+                        if (!filter) return true;
+                        var name = data.name.toLocaleLowerCase();
+                        return name.startsWith(filter.toLocaleLowerCase());
+                    }).map((data) => (
+                        <NavLink to={`/books/${data.number}${location.search}`} key={data.number} style={({ isActive }) => {
+                            return {
+                                textDecoration: "none",
+                                display: "block",
+                                color: isActive ? "red" : ""
                             }
                         }}>
                             {data.name}
--- src/data/data.js
+++ src/data/data.js
@@ -16,7 +16,43 @@ const mydata=[
         number:2,
         amout:70000,
         due:"1394,5,7"
-    }
+    },
+    {
+        name:"هزار و دو شب",
+        number:6,
+        amout:9500,
+        due:"1300,5,7"
+    },
+    {
+        name:"شبلیز",
+        number:10,
+        amout:100000,
+        due:"1290,5,7"
+    },
+    {
+        name:"شبخدز",
+        number:111,
+        amout:760,
+        due:"1340,5,7"
+    },
+    {
+        name :"صبح قهوه",
+        number:22,
+        amout:72000,
+        due:"1294,5,7"
+    },
+    {
+        name:"ناردون",
+        number:90,
+        amout:9500,
+        due:"1396,5,7"
+    },
+    {
+        name:"سیاه سفید",
+        number:17,
+        amout:78000,
+        due:"1370,5,7"
+    },
 ]
 export function Data(){ //extract mydata
   return mydata
