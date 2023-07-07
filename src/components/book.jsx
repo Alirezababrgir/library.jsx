3f81593e3de9d159be640c59151b2741026daf31
Alirezababrgir <zakhargithub021@gmail.com>
2023-07-07 09:20:58 -0400

    create delet button for book -66

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,17 +1,23 @@
-import { useParams } from "react-router-dom";
-import { bid } from "../data/data";
-const Showbook=()=>{
-    const params=useParams();
-    const B=bid(parseInt(params.BookID))
-return(
-<div class="card">
-    <h5>نام کتاب:{B.name}</h5>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">شماره شناسه:{params.BookID}</li>
-    <li class="list-group-item">تاریخ انقضا:{B.due}</li>
-    <li class="list-group-item">قیمت این کتاب {B.amout}میباشد</li>
-  </ul>
-</div>
-)
+import { useParams, useLocation,useNavigate } from "react-router-dom";
+import { bid, deletdata } from "../data/data";
+const Showbook = () => {
+  const location = useLocation();
+  const navigate=useNavigate();
+  const params = useParams();
+  const B = bid(parseInt(params.BookID))
+  return (
+    <div class="card">
+      <h5>نام کتاب:{B.name}</h5>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">شماره شناسه:{params.BookID}</li>
+        <li class="list-group-item">تاریخ انقضا:{B.due}</li>
+        <li class="list-group-item">قیمت این کتاب {B.amout}میباشد</li>
+        <li><button onClick={() => {
+          deletdata(B.number)
+          navigate("/books"+location.search)
+        }}>حذف کتاب</button></li>
+      </ul>
+    </div>
+  )
 }
 export default Showbook;
